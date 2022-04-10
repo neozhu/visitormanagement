3ad69ac13cb61367a0ea0498f4cff7f7dadbb001
neozhu <neo.js.cn@gmail.com>
2022-04-10 10:36:54 +0800

    wip

--- src/Application/Features/VisitorHistories/Commands/Create/CreateVisitorHistoryCommand.cs
+++ src/Application/Features/VisitorHistories/Commands/Create/CreateVisitorHistoryCommand.cs
@@ -3,36 +3,49 @@
 
 using CleanArchitecture.Blazor.Application.Features.VisitorHistories.DTOs;
 using CleanArchitecture.Blazor.Application.Features.VisitorHistories.Caching;
+using CleanArchitecture.Blazor.Application.Features.Visitors.Caching;
 
 namespace CleanArchitecture.Blazor.Application.Features.VisitorHistories.Commands.Create;
 
-    public class CreateVisitorHistoryCommand: VisitorHistoryDto,IRequest<Result<int>>, IMapFrom<VisitorHistory>, ICacheInvalidator
+public class CreateVisitorHistoryCommand : VisitorHistoryDto, IRequest<Result<int>>, IMapFrom<VisitorHistory>, ICacheInvalidator
+{
+    public string CacheKey => VisitorHistoryCacheKey.GetAllCacheKey;
+    public CancellationTokenSource? SharedExpiryTokenSource => VisitorHistoryCacheKey.SharedExpiryTokenSource;
+}
+
+public class CreateVisitorHistoryCommandHandler : IRequestHandler<CreateVisitorHistoryCommand, Result<int>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+    private readonly IStringLocalizer<CreateVisitorHistoryCommand> _localizer;
+    public CreateVisitorHistoryCommandHandler(
+        IApplicationDbContext context,
+        IStringLocalizer<CreateVisitorHistoryCommand> localizer,
+        IMapper mapper
+        )
     {
-      public string CacheKey => VisitorHistoryCacheKey.GetAllCacheKey;
-      public CancellationTokenSource? SharedExpiryTokenSource => VisitorHistoryCacheKey.SharedExpiryTokenSource;
+        _context = context;
+        _localizer = localizer;
+        _mapper = mapper;
     }
-    
-    public class CreateVisitorHistoryCommandHandler : IRequestHandler<CreateVisitorHistoryCommand, Result<int>>
+    public async Task<Result<int>> Handle(CreateVisitorHistoryCommand request, CancellationToken cancellationToken)
     {
-        private readonly IApplicationDbContext _context;
-        private readonly IMapper _mapper;
-        private readonly IStringLocalizer<CreateVisitorHistoryCommand> _localizer;
-        public CreateVisitorHistoryCommandHandler(
-            IApplicationDbContext context,
-            IStringLocalizer<CreateVisitorHistoryCommand> localizer,
-            IMapper mapper
-            )
+        var item = _mapper.Map<VisitorHistory>(request);
+        _context.VisitorHistories.Add(item);
+        var visitor = await _context.Visitors.FirstAsync(x => x.Id == request.VisitorId);
+        if (visitor.CheckinDate is null)
         {
-            _context = context;
-            _localizer = localizer;
-            _mapper = mapper;
+            visitor.CheckinDate = item.TransitDateTime;
+            
         }
-        public async Task<Result<int>> Handle(CreateVisitorHistoryCommand request, CancellationToken cancellationToken)
+        if (item.CheckinPointId >= 2 && visitor.CheckoutDate is null)
         {
-           var item = _mapper.Map<VisitorHistory>(request);
-           _context.VisitorHistories.Add(item);
-           await _context.SaveChangesAsync(cancellationToken);
-           return  Result<int>.Success(item.Id);
+            visitor.CheckoutDate = item.TransitDateTime;
         }
+        
+        VisitorCacheKey.SharedExpiryTokenSource.Cancel();
+        await _context.SaveChangesAsync(cancellationToken);
+        return Result<int>.Success(item.Id);
     }
+}
 
