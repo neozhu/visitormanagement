d94527defa2f0256a9c810aba0d091a66e883673
neozhu <neo.js.cn@gmail.com>
2022-05-08 08:49:55 +0800

    fixed cache token cancel issue and change dark style

--- src/Infrastructure/Services/Picklist/PicklistService.cs
+++ src/Infrastructure/Services/Picklist/PicklistService.cs
@@ -1,59 +1,70 @@
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using CleanArchitecture.Blazor.Application.Features.KeyValues.Caching;
 using CleanArchitecture.Blazor.Application.Features.KeyValues.DTOs;
 using CleanArchitecture.Blazor.Application.Features.KeyValues.Queries.ByName;
+using LazyCache;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitecture.Blazor.Infrastructure.Services.Picklist;
 
-public class PicklistService: IPicklistService
+public class PicklistService : IPicklistService
 {
+    private const string PicklistCacheKey = "PicklistCache";
     private readonly SemaphoreSlim _semaphore = new(1, 1);
+    private readonly IAppCache _cache;
     private readonly IApplicationDbContext _context;
     private readonly IMapper _mapper;
 
     public event Action? OnChange;
     public List<KeyValueDto> DataSource { get; private set; } = new();
 
-    public PicklistService(IApplicationDbContext context, IMapper mapper)
+    public PicklistService(
+      IAppCache cache,
+    IApplicationDbContext context, IMapper mapper)
     {
-        
+        _cache = cache;
         _context = context;
         _mapper = mapper;
     }
     public async Task Initialize()
     {
-        if (DataSource.Count > 0) return;
+        //if (DataSource.Count > 0) return;
         await _semaphore.WaitAsync();
         try
         {
+            DataSource = await _cache.GetOrAddAsync(PicklistCacheKey,
+                () => _context.KeyValues.OrderBy(x => x.Name).ThenBy(x => x.Value)
+                    .ProjectTo<KeyValueDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync(),
+                  KeyValueCacheKey.MemoryCacheEntryOptions);
 
-            DataSource = await _context.KeyValues.OrderBy(x => x.Name).ThenBy(x => x.Value)
-          .ProjectTo<KeyValueDto>(_mapper.ConfigurationProvider)
-          .ToListAsync();
-            OnChange?.Invoke();
         }
         finally
         {
             _semaphore.Release();
         }
-       
+
     }
     public async Task Refresh()
     {
         await _semaphore.WaitAsync();
         try
         {
-            DataSource = await _context.KeyValues.OrderBy(x => x.Name).ThenBy(x => x.Value)
-          .ProjectTo<KeyValueDto>(_mapper.ConfigurationProvider)
-          .ToListAsync();
+            _cache.Remove(PicklistCacheKey);
+            DataSource = await _cache.GetOrAddAsync(PicklistCacheKey,
+                () => _context.KeyValues.OrderBy(x => x.Name).ThenBy(x => x.Value)
+                    .ProjectTo<KeyValueDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync(),
+                KeyValueCacheKey.MemoryCacheEntryOptions
+                  );
             OnChange?.Invoke();
         }
         finally
         {
             _semaphore.Release();
         }
-       
+
     }
 }
